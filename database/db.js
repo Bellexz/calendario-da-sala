098467acc7fcdfe3cbe4e897d9a3a0a26d38e25e
Bellexz <isabelle607@gmail.com>
2026-03-06 22:34:45 -0300

    corrige caminho do banco

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const sqlite3 = require("sqlite3").verbose()
 const bcrypt = require("bcrypt")
 
-const db = new sqlite3.Database("./database/calendario.db")
+const db = new sqlite3.Database(__dirname + "/calendario.db")
+const db = require("./database/db.js")
 
 db.serialize(() => {
 
